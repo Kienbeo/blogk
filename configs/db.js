47fb7bfe0aa5446143eba5e4c727ef19552b390c
Vũ Tự Kiên <kienonwork@gmail.com>
2023-10-06 03:56:35 +0000

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,5 @@
-var mongoose = require('mongoose');
-mongoose.set('debug', true);
+var mongoose = require("mongoose");
+mongoose.set("debug", true);
 
 // mongoose.connect('mongodb://root:root1995@ds125362.mlab.com:25362/blogk',{
 // 	useNewUrlParser: true
@@ -8,15 +8,19 @@ mongoose.set('debug', true);
 // 	useNewUrlParser: true
 // });
 // mongoose.connect('mongodb+srv://root:root95@cluster0.uh3qi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-mongoose.connect('mongodb+srv://root:root@cluster0.tgu2z.mongodb.net/myFirstDatabase?authSource=admin&retryWrites=true&w=majority',{
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true,
-  })
+mongoose
+  .connect(
+    "mongodb+srv://root2:root@cluster0.tgu2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    },
+  )
   .then(() => {
-	console.log('Database connected successfully!');
+    console.log("Database connected successfully!");
   })
   .catch((err) => {
-	console.log('Error connecting with error code:', err);
+    console.log("Error connecting with error code:", err);
   });
